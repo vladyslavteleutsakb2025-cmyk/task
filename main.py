9734376ce4d0c3671f10e47370ef25622bf4168e
vladyslavteleutsakb2025-cmyk <vladyslav.teleutsa.kb.2025@lpnu.ua>
2025-09-19 14:32:29 +0300

    create new python file for first task

---
+++ main.py
@@ -0,0 +1,8 @@
+lasr_name = str(input("Введіть Прізвище: "))
+first_name = str(input("Введіть ім'я: "))
+
+y_day = int(input("Введіть рік народження: "))
+
+print(f"{lasr_name} " + f"{first_name} " + f"{y_day}" + " р.н.")
+print()
+print(f"{lasr_name}\n" + f"{first_name}\n" + f"{y_day}" + " р.н.")
